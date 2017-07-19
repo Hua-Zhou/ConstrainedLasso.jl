ac2b2c17bb08fa05d5d9fb0e9af36de05af52e64
Juhyun Kim <juhkim111@ucla.edu>
2017-07-19 11:42:48 -0700

    using Mosek for now

--- src/ConstrainedLasso.jl
+++ src/ConstrainedLasso.jl
@@ -1,9 +1,11 @@
 module ConstrainedLasso
 
-using Convex, SCS, ECOS
+using Convex, Mosek, SCS
 
 # package code goes here
 include("constrsparsereg.jl")
 include("classopath.jl")
+
+export lsq_classopath, lsq_constrsparsereg
 
 end # module
