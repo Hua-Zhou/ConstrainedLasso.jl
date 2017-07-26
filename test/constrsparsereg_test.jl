919609dfce774987fe8ac72e3eb810bb42eb0704
Juhyun Kim <juhkim111@ucla.edu>
2017-07-26 11:18:53 -0700

    envelop the test in a module

--- test/constrsparsereg_test.jl
+++ test/constrsparsereg_test.jl
@@ -1,6 +1,10 @@
+module constrsparsereg_test
+
+using Base.Test, ConstrainedLasso, SCS
+
 info("Test lsq_constrsparsereg: sum-to-zero constraint")
 
-### set up ###
+# set up
 srand(123)
 n, p = 100, 20
 # truth with sum constraint sum(β) = 0
@@ -24,49 +28,18 @@ info("Optimize at a single tuning parameter values")
 ρ = 10.0
 β̂, = lsq_constrsparsereg(X, y, ρ; Aeq = Aeq, beq = beq,
     penwt = penwt, solver = solver)
-#@show β̂
-@test sum(β̂)≈0.0 atol=1e-6
+@test sum(β̂)≈0.0 atol=1e-5
+
 
 info("Optimize at multiple tuning parameter values")
-
 ρlist = 1.0:10.0
 β̂, = lsq_constrsparsereg(X, y, ρlist; Aeq = Aeq, beq = beq,
     penwt = penwt, solver = solver)
-#@show sum(β̂, 1)
-
 @testset "zero-sum for multiple param values" begin
 for si in sum(β̂, 1)
   @test si≈0.0 atol=1.0e-5
 end
 end
 
-info("Test lsq_classopath: sum-to-zero constraint")
-logging(DevNull, ConstrainedLasso, :lsq_classopath, kind=:warn) # hide
-β̂path1, ρpath1, objpath, = lsq_classopath(X, y; Aeq = Aeq, beq = beq, solver = solver)
-@test all(abs.(sum(β̂path1, 1)) .< 1e-6)
 
-# info("Test lsq_classopath: non-negativity constraint")
-#
-# ### set up ###
-# n, p = 20, 100
-# # define true parameter vector: sparsity with a few non-zero coefficients
-# β = zeros(p)
-# β[1:10] = 1:10
-# # inequality constraints
-# Aineq = - eye(p)
-# bineq = zeros(p)
-# # generate data
-# srand(41)
-# X = randn(n, p)
-# y = X * β + randn(n)
-# # using Mosek; solver = MosekSolver(MSK_IPAR_BI_MAX_ITERATIONS=10e8);
-#
-# β̂path2, ρpath2, = lsq_classopath(X, y; Aineq = Aineq, bineq = bineq,
-#           solver = solver)
-# #@test all(β̂path2 .>= -0.5)
-#
-# @testset "non-negativity" begin
-# for i in reshape(β̂path2, p * length(ρpath2), 1)
-#   @test i >= 0
-# end
-# end
+end # constrsparsereg_test module
