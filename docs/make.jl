7ee8e263b6e59044b396a11729bd285c2345218a
Juhyun Kim <juhkim111@ucla.edu>
2017-08-05 04:52:15 -0700

    add interface page for testing

--- docs/make.jl
+++ docs/make.jl
@@ -1,35 +1,35 @@
 using Documenter, ConstrainedLasso
 
-makedocs(
-#      doctest   = false,
-      format    = :html,
-      clean     = true,
-      sitename  = "ConstrainedLasso.jl",
-      modules   = [ConstrainedLasso],
-      pages     = [
-          "Home"     => "index.md",
-	  "Interface" => "interface.md",
-          "References" => "references.md"
-      ]
-
-)
-
-deploydocs(
-      repo    = "github.com/Hua-Zhou/ConstrainedLasso.jl.git",
-      target  = "build",
-      julia   = "0.6",
-      deps    = nothing,
-      make    = nothing
-)
-
-
-# ENV["DOCUMENTER_DEBUG"] = "true"
+# makedocs(
+# #      doctest   = false,
+#       format    = :html,
+#       clean     = true,
+#       sitename  = "ConstrainedLasso.jl",
+#       modules   = [ConstrainedLasso],
+#       pages     = [
+#           "Home"     => "index.md",
+# 	  "Interface" => "interface.md",
+#           "References" => "references.md"
+#       ]
 #
-# makedocs()
+# )
 #
 # deploydocs(
-#     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-#     repo   = "github.com:Hua-Zhou/ConstrainedLasso.jl.git",
-#     julia  = "0.6",
-#     osname = "osx"
+#       repo    = "github.com/Hua-Zhou/ConstrainedLasso.jl.git",
+#       target  = "build",
+#       julia   = "0.6",
+#       deps    = nothing,
+#       make    = nothing
 # )
+
+
+ENV["DOCUMENTER_DEBUG"] = "true"
+
+makedocs()
+
+deploydocs(
+    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+    repo   = "github.com:Hua-Zhou/ConstrainedLasso.jl.git",
+    julia  = "0.6",
+    osname = "osx"
+)
