d1e00c316321a8fa814d3d0c64673c970f0b4ea2
Juhyun Kim <juhkim111@ucla.edu>
2017-08-04 23:51:31 -0700

    comment out mkdocs for testing

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,34 @@
 using Documenter, ConstrainedLasso
 
-ENV["DOCUMENTER_DEBUG"] = "true"
+makedocs(
+      doctest   = false,
+      format    = :html,
+      clean     = true,
+      sitename  = "ConstrainedLasso.jl",
+      modules   = [ConstrainedLasso],
+      pages     = [
+          "Home"     => "index.md",
+          "References" => "references.md"
+      ]
 
-makedocs()
+)
 
 deploydocs(
-    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-    repo   = "github.com:Hua-Zhou/ConstrainedLasso.jl.git",
-    julia  = "0.6",
-    osname = "osx"
+      repo    = "github.com/Hua-Zhou/ConstrainedLasso.jl.git",
+      target  = "build",
+      julia   = "0.6",
+      deps    = nothing,
+      make    = nothing
 )
+
+
+# ENV["DOCUMENTER_DEBUG"] = "true"
+#
+# makedocs()
+#
+# deploydocs(
+#     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+#     repo   = "github.com:Hua-Zhou/ConstrainedLasso.jl.git",
+#     julia  = "0.6",
+#     osname = "osx"
+# )
