9ea60025ece6100fb9a68684ad2aebcb5133f9a9
Hua Zhou <huazhou@ucla.edu>
2017-08-04 17:19:41 -0700

    add mkdocs.yml

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,22 @@
 using Documenter, ConstrainedLasso
 
-makedocs(
-      doctest   = false,
-      format    = :html,
-      clean     = true,
-      sitename  = "ConstrainedLasso.jl",
-      modules   = [ConstrainedLasso],
-      pages     = [
-          "Home"     => "index.md",
-          "References" => "references.md"
-      ]
+ENV["DOCUMENTER_DEBUG"] = "true"
 
+makedocs(
+    doctest   = false,
+    format    = :html,
+    clean     = true,
+    sitename  = "ConstrainedLasso.jl",
+    modules   = [ConstrainedLasso],
+    pages     = [
+        "Home"     => "index.md",
+        "References" => "references.md"
+    ]
 )
 
 deploydocs(
-      repo    = "github.com/Hua-Zhou/ConstrainedLasso.jl.git",
-      target  = "build",
-      julia   = "0.6",
-      deps    = nothing,
-      make    = nothing
+    deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
+    repo    = "github.com/Hua-Zhou/ConstrainedLasso.jl.git",
+    julia   = "0.6",
+    osname  = "osx"
 )
