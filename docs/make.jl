a7b02fcc38bdbfbb943b2e3dad34f4b78fec9e9a
Hua Zhou <huazhou@ucla.edu>
2017-08-04 17:58:01 -0700

    tweak make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,21 +2,11 @@ using Documenter, ConstrainedLasso
 
 ENV["DOCUMENTER_DEBUG"] = "true"
 
-makedocs(
-    doctest   = false,
-    format    = :html,
-    clean     = true,
-    sitename  = "ConstrainedLasso.jl",
-    modules   = [ConstrainedLasso],
-    pages     = [
-        "Home"     => "index.md",
-        "References" => "references.md"
-    ]
-)
+makedocs()
 
 deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
-    repo    = "github.com/Hua-Zhou/ConstrainedLasso.jl.git",
-    julia   = "0.6",
-    osname  = "osx"
+    repo   = "github.com/Hua-Zhou/ConstrainedLasso.jl.git",
+    julia  = "0.6",
+    osname = "osx"
 )
