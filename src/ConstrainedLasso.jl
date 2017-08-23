54b51d9979b959a385aec0e4b1385746d25050da
Juhyun Kim <juhkim111@ucla.edu>
2017-08-23 06:05:06 -0700

    remove SCS

--- src/ConstrainedLasso.jl
+++ src/ConstrainedLasso.jl
@@ -1,12 +1,13 @@
 module ConstrainedLasso
 
-using Convex, SCS, GLMNet, ECOS 
+using Convex, GLMNet, ECOS
 
 # package code goes here
 include("constrsparsereg.jl")
 include("classopath.jl")
 include("constrsparsereg_admm.jl")
+#include("genlasso.jl")
 
-export lsq_classopath, lsq_constrsparsereg, lsq_constrsparsereg_admm
+export lsq_classopath, lsq_constrsparsereg, lsq_constrsparsereg_admm #, genlasso
 
 end # module
